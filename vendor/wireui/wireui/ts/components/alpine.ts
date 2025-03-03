cc674ad382fe861366fe7fa6f842dd91cd05170e
FatihK61 <ffperl76@gmail.com>
2025-03-04 01:26:27 +0300

    drt
    fgjh

--- vendor/wireui/wireui/ts/components/alpine.ts
+++
@@ -1,5 +0,0 @@
-export type Entangle = any
-
-export interface Alpine {
-  data (name: string, data: any): void
-}
--- vendor/wireui/wireui/webpack.mix.js
+++
@@ -1,9 +0,0 @@
-const mix = require('laravel-mix')
-
-mix.ts('ts/index.ts', 'dist/wireui.js')
-  .setPublicPath('dist')
-  .postCss('resources/css/wireui.css', 'dist', [require('tailwindcss')])
-
-if (mix.inProduction()) {
-  mix.version()
-}
